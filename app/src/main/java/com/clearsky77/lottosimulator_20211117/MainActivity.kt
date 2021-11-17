958c352c1cdc89b315e2932d1d9081f1ad846e17
clearsky77 <clearsky3264@gmail.com>
2021-11-17 09:44:12 +0900

    Initial commit

---
+++ app/src/main/java/com/clearsky77/lottosimulator_20211117/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.lottosimulator_20211117
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
