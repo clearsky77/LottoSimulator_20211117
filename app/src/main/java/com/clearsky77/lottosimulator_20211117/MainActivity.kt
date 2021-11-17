99525047ff9bc4997a3588b423a3f38bcd5b6504
clearsky77 <clearsky3264@gmail.com>
2021-11-17 11:01:18 +0900

    맞힌 숫자 개수 확인

--- app/src/main/java/com/clearsky77/lottosimulator_20211117/MainActivity.kt
+++ app/src/main/java/com/clearsky77/lottosimulator_20211117/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     val mWinLottoNumArr = ArrayList<Int>()
     lateinit var mLottoNumTxtList : ArrayList<TextView>
     var mBonusNum = 0
+    val mMyLottoNumArr = arrayListOf( 5, 17, 26, 30, 36, 42 )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +25,28 @@ class MainActivity : AppCompatActivity() {
                 makeLottoNumbers()
 //            보너스번호 생성
                 makeBonusNum()
+//            등수 판정
+                checkLottoRank()
             }
         }
+
+    private fun checkLottoRank() {
+        //        내 숫자 6개가 -> 당첨번호 6개중 몇개나 맞췄는가?
+
+        var correctCount = 0
+
+        for ( myNum  in  mMyLottoNumArr) {
+
+//            맞췄는가? -> myNum이  당첨번호에 들어있는가?
+            if ( mWinLottoNumArr.contains(myNum) ) {
+//                맞춘 갯수 증가
+                correctCount++
+            }
+
+        }
+
+        Log.d("맞춘 갯수", "${correctCount}개 맞춤")
+    }
 
 
     fun makeBonusNum() {
