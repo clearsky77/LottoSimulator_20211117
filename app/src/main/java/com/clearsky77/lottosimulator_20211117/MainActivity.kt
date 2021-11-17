b353d035cb12f4f0253d0013559b9fb158989fb2
clearsky77 <clearsky3264@gmail.com>
2021-11-17 10:07:37 +0900

    코틀린 반복문 (for) 사용

--- app/src/main/java/com/clearsky77/lottosimulator_20211117/MainActivity.kt
+++ app/src/main/java/com/clearsky77/lottosimulator_20211117/MainActivity.kt
@@ -2,6 +2,8 @@ package com.clearsky77.lottosimulator_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupEvents() {
-
+        btnBuyLotto.setOnClickListener {
+            // 로또 번호 6개 생성
+            makeLottoNumbers()
+            // 보너스 번호 생성
+        }
     }
 
     private fun setValue() {
 
     }
+
+
+
+    fun makeLottoNumbers(){
+        for (i in 0 until 6){ // for (i in 0..4)
+            Log.d("숫자 확인",i.toString())
+        }
+    }
+
+
 }
